ed29965293488082d6b34926ec75b73422421639
Miguel Halys <mhalys91@gmail.com>
2015-07-25 17:08:05 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -62,9 +62,13 @@ sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia',
+      Quiz.bulkCreate(
+        [{ pregunta: 'Capital de Italia',
       	            respuesta: 'Roma'
-      	         })
+      	         },
+         { pregunta:'Capital de Portugal',
+                    respuesta:'Lisboa'}
+        ])
       .then(function(){console.log('Base de datos inicializada')});
     };
   });
