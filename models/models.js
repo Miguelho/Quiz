936b2dadda9fe34fa5d790545c0ff2d93cd20a28
Miguel Halys <mhalys91@gmail.com>
2015-07-25 13:57:58 +0200

    Despliegue heroku v1.4

--- models/models.js
+++ models/models.js
@@ -29,7 +29,7 @@ var sequelize = new Sequelize(DB_name, user, pwd,
             port:     port,
             host:     host,
             storage:  storage,  // solo SQLite (.env)
-            omitNull: true      // solo Postgres
+            omitNull: true    // solo Postgres
           }
     );
 
