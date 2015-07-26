e2a7d563f8876d3553351a7c4f19943dddbd5562
Miguel Halys <mhalys91@gmail.com>
2015-07-26 18:47:13 +0200

    Validacion entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -40,3 +40,20 @@ exports.answer = function(req, res) {
 exports.author = function(req, res) {
    res.render('author',{title:'Créditos'});
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  })   // res.redirect: Redirección HTTP a lista de preguntas
+};
