7aace65ecc2dd9255c0c48c0733a35cca7384801
Miguel Halys <mhalys91@gmail.com>
2015-07-26 15:37:37 +0200

    Busqueda v1.0

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,10 +14,15 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
+
+  models.Quiz.findAll({where:['pregunta like ?', "%"+req.query.search+"%"]
+  , order: '`pregunta` ASC'}
+  ).then(function(quizes) {
    res.render('quizes/index', { quizes: quizes});
  }
 ).catch(function(error) { next(error);})
+
+
 };
 
 // GET /quizes/question
