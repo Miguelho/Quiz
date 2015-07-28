7332aa009e6e02ee943fa5c65ce6695589abd91c
Miguel Halys <mhalys91@gmail.com>
2015-07-28 19:38:29 +0200

    comentarios

---
+++ controllers/comment_controller.js
@@ -0,0 +1,22 @@
+var models= require('../models/models.js');
+
+// GET /quizes/:quizId/cooments/new
+exports.new = function(req, res) {
+  res.render('comments/new.ejs', {quizId: req.params.quizId, errors: []});
+};
+
+// POST /quizes/:quizId/commentss
+exports.create = function(req, res) {
+  var comment = models.Comment.build( {
+      texto: req.body.comment.texto,
+      QuizId: req.params.quizId
+  });
+  comment.validate().then(function(err){
+    if (err) {
+      res.render('comments/new.ejs', {comment: comment, errors: err.errors});
+    } else {
+      comment // save: guarda en DB campos texto de comment
+      .save().then(function(){ res.redirect('/quizes/' + req.params.quizId)})
+    }      // res.redirect: Redirección HTTP a lista de preguntas
+  }).catch(function(error){next(error)});
+};
